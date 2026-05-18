12b4672922d17fb766311ea97bd7c741d31e20fa
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-05-18 16:35:21 +0700

    menambahkan ui baru yaitu fitur search

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-// Toggle class active
+// Toggle class active untuk Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active untuk Search Form
+const searchForm = document.querySelector("#search-form");
+const searchBox = document.querySelector("#search-box");
+
+// ketika Search Button di klik
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik diluar element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
